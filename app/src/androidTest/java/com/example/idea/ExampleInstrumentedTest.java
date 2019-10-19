a0a4c4dbf1c2a8e4375ff288b83103da710d545b
kyleo83 <kyleostling@gmail.com>
2019-10-19 16:34:17 -0700

    Replaced instrumented test

--- app/src/androidTest/java/com/example/idea/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/idea/ExampleInstrumentedTest.java
@@ -4,16 +4,9 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import android.support.test.rule.ActivityTestRule;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -23,8 +16,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Rule
-    public ActivityTestRule<MainActivity> activityTestRule
-            = new ActivityTestRule<>(MainActivity.class, true, true);
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
 
+        assertEquals("com.example.idea", appContext.getPackageName());
+    }
 }
