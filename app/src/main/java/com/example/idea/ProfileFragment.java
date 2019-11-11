4c0ef16c6dafdd780825bb271ec9262eb045783d
robezal <robezal@gmail.com>
2019-11-10 21:05:51 -0800

    fixed issue with edit button.

--- app/src/main/java/com/example/idea/ProfileFragment.java
+++ app/src/main/java/com/example/idea/ProfileFragment.java
@@ -8,7 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+//import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
